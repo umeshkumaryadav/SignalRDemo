1c6c3def2904be2155ba7688763690bed3f4ee0e
umeshkumaryadav <iumeshpro@gmail.com>
2026-09-19 17:57:04 +0530

    Add GitHub Actions test

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -7,7 +7,6 @@ namespace SignalRDemo
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string? Summary { get; set; }
     }
 }
